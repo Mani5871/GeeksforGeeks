a3c0445aea6f28cb63e7774e76974d5c7c82b984
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-02-01 16:15:33 +0530

    Create Middle element.cpp

---
+++ LinkedList/Middle element.cpp
@@ -0,0 +1,27 @@
+int getMiddle(Node *head)
+{
+   // Your code here
+   Node * temp = head;
+   
+   int cnt = 0;
+   
+   while(temp != NULL)
+   {
+       temp = temp -> next;
+       cnt ++;
+   }
+   
+   int cnt1 = cnt / 2 + 1, i = 1;
+   
+   temp = head;
+   
+   while(i < cnt1)
+   {
+       temp = temp -> next;
+       i ++;
+   }
+   
+   return temp -> data;
+   
+   
+}
