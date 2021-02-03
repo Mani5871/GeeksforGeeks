f22cb3c0d5925ffb67e003e20774e5cc483c76b7
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-02-03 18:15:13 +0530

    Create starting point of loop.cpp
    Check for problem in leetcode

---
+++ LinkedList/starting point of loop.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        if(!head || !head -> next)
+            return NULL;
+        
+        ListNode *slow, *fast, *slow1;
+        
+        slow = fast = head;
+        
+        while(fast && fast -> next)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            
+            if(slow == fast)
+            {
+                fast = head;
+                while(slow != fast)
+                {
+                    slow = slow -> next;
+                    fast = fast -> next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+        
+        
+    }
+};
