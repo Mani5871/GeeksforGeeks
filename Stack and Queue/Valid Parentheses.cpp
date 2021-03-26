104f300bbffa432c6030d196bbbc12d3558d9de0
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-03-26 14:10:34 +0530

    Create Valid Parentheses.cpp

---
+++ Stack and Queue/Valid Parentheses.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>str1;
+        
+        int i, j, k, n = s.size();
+        
+        for(i = 0; i < n; i ++)
+        {
+            if(s[i] == '{')
+                str1.push(s[i]);
+            
+            else if(s[i] == '(')
+                str1.push(s[i]);
+            
+            else if(s[i] == '[')
+                str1.push(s[i]);
+            
+            else if(s[i] == ')')
+            {
+                if(str1.empty())
+                    return false;
+                if(str1.top() != '(')
+                    return false;
+                str1.pop();
+            }
+            
+            else if(s[i] == ']')
+            {
+                if(str1.empty())
+                    return false;
+                if(str1.top() != '[')
+                    return false;
+                str1.pop();
+            }
+            
+            else if(s[i] == '}')
+            {
+                if(str1.empty())
+                    return false;
+                if(str1.top() != '{')
+                    return false;
+                str1.pop();
+            }
+                
+        }
+        if(str1.empty())
+            return true;
+        return false;
+        
+    }
+};
