c1d97f76459dc39b211f7b2454ba177fff90627f
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-05-21 09:29:00 +0530

    Update Hash sort.cpp

--- Arrays/Hash sort.cpp
+++ Arrays/Hash sort.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
+typedef long long int lli;
+vector<lli> pos_arr, neg_arr;
 
 int main() {
+    lli i, j, k, n = 10000000, cnt = 0;
     
-    int i, j, k, n;
-    cin >> n; // Input for size of array
-    
-    int arr[n]; // Array declartion
+    vector<lli> arr;
     
     for(i = 0; i < n; i ++)
-        cin >> arr[i]; // Array input
+    {
+        arr.push_back(rand() % n);
+    }
     
+    for(i = 1; i < arr.size(); i ++)
+        if(arr[i] < arr[i - 1])
+            cnt ++;
+    cout << "Total anomalies before sorting " << cnt << endl;
     
-    int pos_max = INT_MIN, neg_max = INT_MAX, neg = 0, pos = 0;
+    auto start = high_resolution_clock::now();
+    
+    lli pos_max = INT_MIN, neg_max = INT_MAX, neg = 0, pos = 0;
     
     // Finding out highest positive number and least negative number
     
@@ -27,11 +37,13 @@ int main() {
     }
     
     // Declaring 2 separate arrays to store the count of given positive and negative integers
-    
-    int pos_arr[pos_max + 1], neg_arr[abs(neg_max) + 1];
-    
+
+   
     for(i = 0; i < pos_max + 1; i ++)
-        pos_arr[i] = neg_arr[i] = 0;
+        pos_arr.push_back(0);
+        
+    for(i = 0; i < neg_max; i ++)
+        neg_arr.push_back(0);
     
     // Finding number of negative and positive integers and finding their counts in respective array
     
@@ -80,6 +92,12 @@ int main() {
             i ++;
     }
     
-    for(i = 0; i < n; i ++)
-        cout << arr[i] << endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << " Time to taken for this algorithm in milli seconds is " << duration.count() << endl;
+    
+    for(i = 1; i < arr.size(); i ++)
+        if(arr[i] < arr[i - 1])
+            cout << "1";
+
 }
